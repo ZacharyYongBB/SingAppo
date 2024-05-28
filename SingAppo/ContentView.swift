bc35a23f73d7519fa047418c7ba25a072c2f5efb
Zachary <zacharyyong_1997@outlook.com>
2024-05-28 20:39:30 +0800

    Initial Commit

---
+++ SingAppo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SingAppo
+//
+//  Created by Zachary on 28/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SingAppo/SingAppoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SingAppoApp.swift
+//  SingAppo
+//
+//  Created by Zachary on 28/5/24.
+//
+
+import SwiftUI
+
+@main
+struct SingAppoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
